d76794f7c34655f693c92b448fb1f7278c26c76d
Sabrina Quintanilla <sabrinalorenQ@gmail.com>
2020-06-15 15:54:45 -0500

    simplified fetch

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,7 +1,6 @@
 import Layout from "../../components/layout";
 import Head from "next/dist/next-server/lib/head";
 import React from "react";
-import {getAllArticleIds, getArticle} from "../../lib/article/article";
 
 export default function articles({ article }) {
     return (
@@ -25,8 +24,8 @@ export default function articles({ article }) {
 }
 
 export async function getStaticProps({ params }) {
-    const res = await fetch('https://localhost:8080/api/${id}')
-    const article = await getArticle(params.id);
+    const res = await fetch('https://localhost:8080/api/articles/${params.id}')
+    const article = res.json();
     return {
         props: {
             article,
@@ -34,40 +33,4 @@ export async function getStaticProps({ params }) {
     };
 }
 
-export async function getStaticPaths() {
-    const res = await fetch('https://localhost:8080/api/articles')
-    //const paths = getAllArticleIds();
-    const paths = articles.map((article) => ({
-        params: { id: article.id },
-     }))
-    return {
-        paths,
-        fallback: false,
-    };
-}
-
-// // This also gets called at build time
-// export async function getStaticProps({ params }) {
-//     // params contains the post `id`.
-//     // If the route is like /posts/1, then params.id is 1
-//     const res = await fetch(`https://.../posts/${params.id}`)
-//     const post = await res.json()
-//
-//     // Pass post data to the page via props
-//     return { props: { post } }
-// }
-// // This function gets called at build time
-// export async function getStaticPaths() {
-//     const res = await fetch('https://.../posts')
-//     const posts = await res.json()
-//
-//     // Get the paths we want to pre-render based on posts
-//     const paths = posts.map((post) => ({
-//         params: { id: post.id },
-//     }))
-//
-//     // We'll pre-render only these paths at build time.
-//     // { fallback: false } means other routes should 404.
-//     return { paths, fallback: false }
-// }
-
+//serverside rendered
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from "../components/layout";
 import React from "react";
-import {getArticlesForHome} from "../lib/article/article";
+import Link from "next";
 
 export async function getStaticProps() {
-    const allArticles = getArticlesForHome();
+    const res = await fetch('https://localhost:8080/api/articles/')
+    const allArticles = res.json();
     return {
         props: {
             allArticles,
-        },
-    };
+        }
+    }
 }
 
- // this is the home page
-export default function Home( ) {
+// this is the home page
+export default function Home({allArticles}) {
     return (
         <Layout home>
             <Head>
@@ -27,6 +28,21 @@ export default function Home( ) {
             </section>
             <section>
                 <p>This section will display Article cards with routes to [id].</p>
+                <h2>Articles</h2>
+                <ul>
+                    {allArticles.map(({id, date, title}) => (
+                        <li key={id}>
+                            <Link href="./articles/[id]" as={`/articles/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br/>
+                            <small>
+                                {/*// make a date component*/}
+                                {/*<Date dateString={date} />*/}
+                            </small>
+                        </li>
+                    ))}
+                </ul>
 
             </section>
 
