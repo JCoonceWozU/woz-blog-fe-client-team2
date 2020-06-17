59ec23d8e39e369d1ca118e7e2c1214bf0eb0ba9
Sabrina Quintanilla <sabrinalorenQ@gmail.com>
2020-06-17 01:50:33 -0500

    created articles structure

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,26 +1,27 @@
 import Head from 'next/head'
-import Layout, {siteTitle} from "../components/layout";
 import React from "react";
+import Layout, {siteTitle} from "../../components/layout";
 
- // this is the home page: "/"
-const Home = () => {
+// this is the articles list page: "/articles"
+const Articles = () => {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
+
             </Head>
 
             <section>
                 <h1>
-                    {siteTitle}
+                    <h1>Articles</h1>
                 </h1>
             </section>
             <section>
-                <p>This section will display Article titles with snippets and links to each [id]</p>
+                <p>This section will display Article titles with links to each [id].</p>
             </section>
 
         </Layout>
     )
 }
 
-export default Home;
+export default Articles;
